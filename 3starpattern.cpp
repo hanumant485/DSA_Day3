c82aab15769d81e3f34a6a977ad39b1cc57b4845
hanumant upnal <hanumanthaupnal@gmail.com>
2026-07-07 14:27:05 +0530

    came across nested loop

--- 3starpattern.cpp
+++ 3starpattern.cpp
@@ -19,26 +19,27 @@ for(int j=1; j<=5; j++){
 }
 cout<<endl;
 
-for(int i=1; i<5; i++){
+for(int i=1; i<=1; i++){
     cout<<"*";
 }
 cout<<endl;
-for(int i=1; i<4; i++){
+for(int i=1; i<=2; i++){
     cout<<"*";
 }
 cout<<endl;
-for(int i=1; i<3; i++){
+for(int i=1; i<=3; i++){
     cout<<"*";
 }
 cout<<endl;
-for(int i=1; i<2; i++){
+for(int i=1; i<=4; i++){
     cout<<"*";
 }
 cout<<endl;
-for(int i=1; i<1; i++){
+for(int i=1; i<=5; i++){
     cout<<"*";
 }
 cout<<endl;
+
 
 for(int j=5; j>=1; j--){
     for(int i=1; i<=j; i++){
@@ -46,11 +47,23 @@ for(int j=5; j>=1; j--){
     }
     cout<<endl;
 }
-
-for(int i=0; i<4; i++){
-    cout<<" ";
+for(int i=1; i<=5; i++){
+    cout<<"*";
 }
-for(int i=0; i<1; i++){
+cout<<endl;
+for(int i=1; i<=4; i++){
+    cout<<"*";
+}
+cout<<endl;
+for(int i=1; i<=3; i++){
+    cout<<"*";
+}
+cout<<endl;
+for(int i=1; i<=2; i++){
+    cout<<"*";
+}
+cout<<endl;
+for(int i=1; i<=1; i++){
     cout<<"*";
 }
 cout<<endl;
@@ -65,6 +78,51 @@ for(int i=4; i>=0; i--){
     cout<<endl;
 }
 
+for(int j=1; j<=4; j++){
+    cout<<" ";
+}
+for(int j=1; j<=1; j++){
+    cout<<"*";
+}
+cout<<endl;
+
+for(int j=1; j<=3; j++){
+    cout<<" ";
+}
+for(int j=1; j<=2; j++){
+    cout<<"*";
+}
+cout<<endl;
+
+for(int j=1; j<=2; j++){
+    cout<<" ";
+}
+for(int j=1; j<=3; j++){
+    cout<<"*";
+}
+cout<<endl;
+
+for(int j=1; j<=1; j++){
+    cout<<" ";
+}
+for(int j=1; j<=4; j++){
+    cout<<"*";
+}
+cout<<endl;
+
+for(int j=1; j<=5; j++){
+    cout<<"*";
+}
+cout<<endl;
+
+
+
+for(int i=1; i<=21; i=i+5){
+    for(int j=i; j<=i+4; j++){
+        cout<<j<<" ";
+    }
+    cout<<endl; 
+}
 for(int j=1; j<=5; j++){
         cout<<j<<" ";
     }
@@ -81,5 +139,6 @@ for(int j=11; j<=15; j++){
     cout<<endl; 
 
     
+
 }
 
